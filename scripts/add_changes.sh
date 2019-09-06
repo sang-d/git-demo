d3d9bd79700e82586a9db8b772d649209262f38e
Sang Dinh <sang.dinh@wizeline.com>
2019-09-06 17:16:31 +0700

    add file

--- scripts/add_changes.sh
+++ scripts/add_changes.sh
@@ -3,13 +3,14 @@
 # creating commit on origin remote 
 echo "adding changes" >> changes.txt
 
-ga changes.txt
-
-gc -m "adding changes"
+gc -am "adding changes"
 
 echo "adding another text" >> changes.txt
 
 gc -am "adding another text"
 
 git push
+
+
+# experiment with fork repo when pushing
 
